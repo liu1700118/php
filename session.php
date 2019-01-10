374d53240fcbce326d5e3ee63f07c2c121aa7e0b
Administrator <1>
2019-01-10 08:54:52 +0800

    1

---
+++ session.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Credentials:true");
+header('Content-type:text/json');
+header("Access-Control-Allow-Origin:http://10.30.29.67:63341");
+//$allow_origin=array(
+//  'http://10.30.29.67:63341'
+//);
+$origin=isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:'';
+if (in_array($origin,$allow_origin)){
+    header('Access-Control-Allow-Origin:'.$origin);
+}
+session_start();
+$message=array('message'=>'success');
+if (!isset($_SESSION['name']))
+{
+    $message['message']='errors';
+    echo json_encode($message);
+}
+else{
+    echo json_encode($message);
+}
+?>
